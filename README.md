2c73b42f12f7585069b442c87680a7ecd2cef3a9
CarlosReategui <carlos.reategui@utec.edu.pe>
2023-01-19 18:22:34 -0500

    subir esquema testing + readme

--- README.md
+++ README.md
@@ -1,81 +1,57 @@
 # FULLSTACK TECHNICAL TEST
-## Creación de un API REST con Django Rest Framework y un client web con React
 
-La prueba consiste en crear un API REST para un único recurso, la cual será consumida desde un cliente web. La generación de todos los archivos necesarios y el manejo de una óptima estructura de archivos debe ser realizado por el/la postulante. Genere un fork de este repositorio. Utilice versiones con sporte de Python, Django y React.
+# Backend
 
+## Pasos para levantar el proyecto
 
-## Requerimientos generales
+- Nota: En caso de contar con Python 3 como default, usar el comando `python3` en lugar de `python` para los comandos.
 
-1. El API REST debe ser desarrollado con Django Rest Framework.
-2. El cliente web debe ser desarrollado con React + Typescript.
-3. El cliente web debe consumir el API REST.
-4. Se debe utilizar el ORM de Django para el modelo de la base de datos.
-5. Se debe crear un archivo `requirements.txt` con las dependencias necesarias para ejecutar el proyecto.
-6. Se debe crear un archivo `README.md` en el cual se explique el funcionamiento del proyecto, así como las instrucciones para ejecutarlo.
-7. Se debe crear un archivo `.env` que contenga las variables de entorno necesarias para la ejecución del proyecto.
-8. Se debe crear un archivo `.gitignore` que ignore los archivos y carpetas innecesarias para la ejecución del proyecto.
+1. `cd backend/`
+2. `python3 -m venv .venv`
+3. `source .venv/bin/activate`
+4. `pip install -r requirements.txt`
+5. `python manage.py migrate`
+6. `python manage.py createsuperuser`
 
+## Rutas
 
-## Requerimientos backend
-1. Generar un endpoint para administrar un albergue de animales rescatados (perros y gatos). Se debe poder llevar un control de los animales en albergue: nombre, edad, raza, tipo (perro o gato), estado (adoptado, en adopción, en espera de adopción, etc). Se debe poder llevar un control de los voluntarios que trabajan en el albergue: nombre, apellido, correo electrónico, contraseña, estado (activo, inactivo, etc). Se debe poder llevar un control de las personas que adoptan animales: nombre, apellido, correo electrónico, contraseña, estado (activo, inactivo, etc). Se debe poder llevar un control de las adopciones, es decir, el registro de cuándo y a quién se le entregó un animal: fecha, animal, voluntario, persona que adopta, estado (finalizado, en proceso, etc).
+- Las rutas disponibles son `/admin` y `/api`.
+- Dentro de `/api` se encuentran todas las rutas REST para los adoptantes, las voluntarios, animales y adopciones.
+- En el archivo `backend/pets_app/urls.py` puede verse a detalle cada una de las rutas.
 
-    **Nota:** El endpoint debe permitir realizar las siguientes operaciones:
-    * Listar todos los animales en albergue.
-    * Listar todos los voluntarios.
-    * Listar todos los adoptantes.
-    * Listar todas las adopciones.
-    * Crear un animal en albergue.
-    * Crear un voluntario.
-    * Crear un adoptante.
-    * Crear una adopción.
-    * Obtener un animal en albergue.
-    * Obtener un voluntario.
-    * Obtener un adoptante.
-    * Obtener una adopción.
-    * Actualizar un animal en albergue.
-    * Actualizar un voluntario.
-    * Actualizar un adoptante.
-    * Actualizar una adopción.
-    * Eliminar un animal en albergue.
-    * Eliminar un voluntario.
-    * Eliminar un adoptante.
-    * Eliminar una adopción.
+## Autenticación
 
-2. El modelo de la base de datos queda a elección del postulante; sin embargo, esta debe tener todo lo necesario para plasmar lo explicado en el requerimiento anterior.
+La aplicación cuenta con un sistema de autenticación basado en tokens usando `djangorestframework_simplejwt`. Para obtener un token, se debe hacer una petición POST a `/api/token/` con los datos de un usuario existente. El token se obtiene en el campo `access` del JSON de respuesta. Para refrescar un token expirado, se debe hacer una petición POST a `/api/token/refresh/` con el token expirado en el campo `refresh` del JSON de la petición. El token refrescado se obtiene en el campo `access` del JSON de respuesta.
 
-3. Se desea validar que los datos recibidos via POST request cumplan las restricciones establecidas en el modelo de la base de datos. Los errores deben mostrarse al usuario en el response.
+## Testing
 
-4. Se desea que el API sea restringido mediante algún método de autenticación. Queda a elección del postulante el approach de autenticación a seguir. El nivel de seguridad será evaluado.
+Los tests de los views del proyecto se encuentran en `backend/pets_app/tests.py` y pueden ser ejecutados con `python manage.py test`.
 
-5. Se desea que el usuario se autentifique mediante json web tokens, para ello se utilizará la librería Simple JWT para Django rest framework.
+## Deployment
 
-## Requerimientos frontend
+URL: [https://pets-app.onrender.com/](https://pets-app.onrender.com/)
 
-1. Debe utilizar React + Typescript junto al framework [Mantine UI](https://mantine.dev/).
+## Credenciales admin
 
-2. Debe contar con un registro solo para los voluntarios. Los administradores (superusers) solo pueden ser creados desde Django admin.
+Pedirlas al autor del proyecto.
 
-3. Debe contar con un Login unificado para los voluntarios, adoptantes y administradores.
+# Frontend
 
-4. Los administradores pueden acceder al CRUD de los animales en albergue, de los voluntarios, de los adoptantes y de las adopciones.
+## Pasos para levantar el proyecto
 
-5. Los adoptantes solo pueden visualizar los animales disponibles para adopción y solicitar la adopción de un animal.
-6. Los voluntarios pueden visualizar los animales del albergue, los adoptantes y las adopciones. Además, pueden cambiar el estado de un animal en albergue a "adoptado" o "en adopción".
+1. `cd frontend/`
+2. `npm install`
+3. `npm start`
 
+## Páginas
 
-## Adicionales
-1. Deployar el cliente web y servidor backend en la capa gratuita de algún servicio cloud.
-2. Realizar proceso de quality assurance con SonarCloud, recuerde que para ello este repositorio debe manteneser público. (Adjuntar link del reporte obtenido)
+- `/`: Página principal.
+- `/login`: Página de login.
+- `/register`: Página de registro.
+- `/admin`: Página de administración (ruta protegida; solo puede acceder un usuario de tipo admin).
+- `/adoptante`: Página de adoptante (ruta protegida; solo puede acceder un usuario de tipo adoptante).
+- `/voluntario`: Página de voluntario (ruta protegida; solo puede acceder un usuario de tipo voluntario).
 
+## Deployment
 
-
-# Objetivos de la prueba
-* Evaluar el conocimiento general del lenguaje Python y de React.
-* Evaluar el conocimiento en arquitecturas web (MVC, REST).
-* Comprobar el conocimiento independientemente de la herramienta / framework.
-* Evaluar buenas prácticas en el código relacionadas con Python (pep8).
-* Evaluar conocimientos de frontend.
-* Evaluar el modelo mental que se tiene para la organización de classes, funciones, módulos.
-* Evaluar el conocimiento de buenas prácticas de desarrollo de software.
-* Evaluar el conocimiento de buenas prácticas de seguridad.
-* Evaluar el conocimiento de testing.
+URL: [https://adoptaunamigo-rho.vercel.app/](https://adoptaunamigo-rho.vercel.app/)
--- backend/pets_app/tests.py
+++ backend/pets_app/tests.py
@@ -1,3 +1,304 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from pets_app.models import User, Adoptante, Voluntario, Animal, Adopcion, UserManager, AdoptanteManager, VoluntarioManager
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+
+        self.client = Client()
+
+        self.admin = User.objects.create_superuser(email='admin@example.com',
+                                                   first_name='Admin',
+                                                   last_name='Admin',
+                                                   password='admin123',
+                                                   role='ADMIN',
+                                                   username='admin')
+
+        self.voluntario = Voluntario.objects.create_user(email='voluntario@example.com',
+                                                         first_name='Voluntario',
+                                                         last_name='Voluntario',
+                                                         password='voluntario123',
+                                                         role='VOLUNTARIO')
+
+        self.adoptante = Adoptante.objects.create_user(email='adoptante@example.com',
+                                                       first_name='Adoptante',
+                                                       last_name='Adoptante',
+                                                       password='adoptante123',
+                                                       role='ADOPTANTE')
+
+        self.animal = Animal.objects.create(nombre='Animal',
+                                            tipo='PERRO',
+                                            raza='Raza',
+                                            edad=1,
+                                            estado='EN_ADOPCION')
+
+        self.adopcion = Adopcion.objects.create(animal=self.animal,
+                                                adoptante=self.adoptante,
+                                                voluntario=self.voluntario)
+
+    def test_jwt_token(self):
+        """Test case for token authentication"""
+
+        response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+
+        refresh = response.data['refresh']
+        access = response.data['access']
+        status_code = response.status_code
+
+        self.assertIsNotNone(refresh)
+        self.assertIsNotNone(access)
+        self.assertEqual(status_code, 200)
+
+    def test_jwt_refresh_token(self):
+        """Test case for token refresh"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+        jwt_refresh = jwt_response.data['refresh']
+
+        response = self.client.post('/api/token/refresh/', {'refresh': jwt_refresh},
+                                    format='json', HTTP_AUTHORIZATION='Bearer ' + jwt_response.data['access'])
+
+        refresh = response.data['refresh']
+        access = response.data['access']
+        status_code = response.status_code
+
+        self.assertIsNotNone(refresh)
+        self.assertIsNotNone(access)
+        self.assertEqual(status_code, 200)
+
+    def test_voluntario_list(self):
+        """Test case for voluntario list"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/voluntarios/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(len(api_response), 1)
+        self.assertEqual(api_response[0]['email'], 'voluntario@example.com')
+
+    def test_adoptante_list(self):
+        """Test case for adoptante list"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/adoptantes/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(len(api_response), 1)
+        self.assertEqual(api_response[0]['email'], 'adoptante@example.com')
+
+    def test_animal_list(self):
+        """Test case for animal list"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/animales/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(len(api_response), 1)
+        self.assertEqual(api_response[0]['nombre'], 'Animal')
+
+    def test_adopcion_list(self):
+        """Test case for adopcion list"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'voluntario@example.com', 'password': 'voluntario123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/adopciones/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(len(api_response), 1)
+
+        self.assertEqual(api_response[0]['animal']['id'], self.animal.id)
+        self.assertEqual(api_response[0]['adoptante']['id'], self.adoptante.id)
+        self.assertEqual(api_response[0]['voluntario']
+                         ['id'], self.voluntario.id)
+
+    def test_voluntario_detail(self):
+        """Test case for voluntario detail"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/voluntarios/' + str(self.voluntario.id) + '/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(api_response['email'], 'voluntario@example.com')
+
+    def test_adoptante_detail(self):
+        """Test case for adoptante detail"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/adoptantes/' + str(self.adoptante.id) + '/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(api_response['email'], 'adoptante@example.com')
+
+    def test_animal_detail(self):
+        """Test case for animal detail"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/animales/' + str(self.animal.id) + '/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(api_response['nombre'], 'Animal')
+
+    def test_adopcion_detail(self):
+        """Test case for adopcion detail"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        response = self.client.get(
+            '/api/adopciones/' + str(self.adopcion.id) + '/', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(api_response['animal']['id'], self.animal.id)
+        self.assertEqual(api_response['adoptante']['id'], self.adoptante.id)
+        self.assertEqual(api_response['voluntario']['id'], self.voluntario.id)
+
+    def test_voluntario_create(self):
+        """Test case for voluntario create"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        data = {
+            'email': 'voluntario_2@example',
+            'password': 'voluntario123',
+            'first_name': 'Voluntario',
+            'last_name': '2',
+            'role': 'VOLUNTARIO'
+        }
+
+        response = self.client.post(
+            '/api/voluntarios/', data, format='json', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 201)
+        self.assertEqual(api_response['email'], 'voluntario_2@example')
+
+    def test_adoptante_create(self):
+        """Test case for adoptante create"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        data = {
+            'email': 'adoptante_2@example',
+            'password': 'adoptante123',
+            'first_name': 'Adoptante',
+            'last_name': '2',
+            'role': 'ADOPTANTE'
+        }
+
+        response = self.client.post(
+            '/api/adoptantes/', data, format='json', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 201)
+        self.assertEqual(api_response['email'], 'adoptante_2@example')
+
+    def test_animal_create(self):
+        """Test case for animal create"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'admin@example.com', 'password': 'admin123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        data = {
+            'nombre': 'Animal 2',
+            'edad': 2,
+            'raza': 'raza 2',
+            'tipo': 'PERRO',
+            'estado': 'EN_ADOPCION'
+        }
+
+        response = self.client.post(
+            '/api/animales/', data, format='json', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 201)
+        self.assertEqual(api_response['nombre'], 'Animal 2')
+
+    def test_adopcion_create(self):
+        """Test case for adopcion create"""
+
+        jwt_response = self.client.post(
+            '/api/token/', {'email': 'adoptante@example.com', 'password': 'adoptante123'}, format='json')
+        jwt_access = jwt_response.data['access']
+
+        data = {
+            'animal_id': self.animal.id,
+            'voluntario_id': self.voluntario.id
+        }
+
+        response = self.client.post(
+            '/api/adoptanteCrearAdopcion/', data, format='json', HTTP_AUTHORIZATION='Bearer ' + jwt_access)
+
+        status_code = response.status_code
+        api_response = response.data
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(api_response['message'],
+                         'Adopcion realizada con exito')
