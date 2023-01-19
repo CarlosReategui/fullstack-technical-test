a8722f7f2cb2dc106eb3918d5b8c394c6719b38d
CarlosReategui <carlos.reategui@utec.edu.pe>
2023-01-19 03:54:43 -0500

    refactorizar appshell

--- frontend/src/components/AdminAppShell/AdminAppShell.tsx
+++ frontend/src/components/AdminAppShell/AdminAppShell.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { AppShell, Navbar, Header, Group, Text } from "@mantine/core";
 import { MainLinks } from "./_mainLinks";
 import { User } from "./_user";
-import { FaPaw } from "react-icons/fa";
+import GeneralAppShell from "../GeneralAppShell/GeneralAppShell";
 
 type Props = {
   children: React.ReactNode;
@@ -10,39 +9,9 @@ type Props = {
 
 const AdminAppShell: React.FC<Props> = ({ children }) => {
   return (
-    <AppShell
-      padding="md"
-      fixed={false}
-      navbar={
-        <Navbar width={{ base: 225 }} p="xs">
-          <Navbar.Section grow mt="xs">
-            <MainLinks />
-          </Navbar.Section>
-          <Navbar.Section>
-            <User />
-          </Navbar.Section>
-        </Navbar>
-      }
-      header={
-        <Header height={60}>
-          <Group sx={{ height: "100%" }} px={20} position="apart">
-            <Group spacing={5}>
-              <FaPaw size={20} />
-              <Text size="xl" weight={500}>
-                AdoptaUnAmigo
-              </Text>
-            </Group>
-          </Group>
-        </Header>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor: theme.colors.gray[0],
-        },
-      })}
-    >
+    <GeneralAppShell mainLinks={<MainLinks />} user={<User />}>
       {children}
-    </AppShell>
+    </GeneralAppShell>
   );
 };
 
---
+++ frontend/src/components/GeneralAppShell/GeneralAppShell.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Group,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+import { FaPaw } from "react-icons/fa";
+
+type Props = {
+  children: React.ReactNode;
+  mainLinks: JSX.Element;
+  user: JSX.Element;
+};
+
+const GeneralAppShell: React.FC<Props> = ({ children, mainLinks, user }) => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return (
+    <AppShell
+      padding="md"
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Navbar.Section grow mt="xs">
+            {mainLinks}
+          </Navbar.Section>
+          <Navbar.Section>{user}</Navbar.Section>
+        </Navbar>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Group sx={{ height: "100%" }} px={20} position="apart">
+              <Group spacing={5}>
+                <FaPaw size={20} />
+                <Text size="xl" weight={500}>
+                  AdoptaUnAmigo
+                </Text>
+              </Group>
+            </Group>
+          </div>
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor: theme.colors.gray[0],
+        },
+      })}
+    >
+      {children}
+    </AppShell>
+  );
+};
+
+export default GeneralAppShell;
